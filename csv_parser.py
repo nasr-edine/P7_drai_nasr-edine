cf099463363e792b5f00c91a30a49abdb451df4d
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-05-27 17:45:02 +0200

    add first version of bruteforce

---
+++ csv_parser.py
@@ -0,0 +1,36 @@
+import csv
+from pathlib import Path
+import sys
+
+
+def printDataSet(items, w, v):
+    print(19 * "-")
+    for i in range(len(items)):
+        print("|", str(items[i]).ljust(4), end='|')
+        print(str(w[i]).ljust(5), end='|')
+        print(str(v[i]).ljust(5), end='|\n')
+        print(19 * "-")
+
+
+def getDataFromCsv(dataFile):
+    items = []
+    w = []
+    v = []
+    with open(dataFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
+        line_count = 0
+        for row in csv_reader:
+            items.append(int(row[0]))
+            w.append(int(row[1]))
+            v.append(int(row[2]))
+            line_count += 1
+    return items, w, v
+
+
+# if len(sys.argv) == 2:
+#     data_folder = Path("csv/")
+#     csv_path = data_folder / sys.argv[1]
+#     items, w, v = getDataFromCsv(csv_path)
+#     printDataSet(items, w, v)
+#     print("row number: ", len(items))
---
+++ knapsack.py
@@ -0,0 +1,59 @@
+import itertools
+import sys
+
+from csv_parser import getDataFromCsv, printDataSet
+from pathlib import Path
+
+if len(sys.argv) == 2:
+    data_folder = Path("csv/")
+    csv_path = data_folder / sys.argv[1]
+    items, w, v = getDataFromCsv(csv_path)
+    # printDataSet(items, w, v)
+    # print("row number: ", len(items))
+    exit()
+
+# item = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# w = [20, 30, 50, 70, 60, 80, 22, 26, 48, 34,
+#      42, 110, 38, 14, 18, 8, 4, 10, 24, 114]
+# v = [5, 10, 15, 20, 17, 25, 7, 11, 13, 27, 17, 9, 23, 1, 3, 8, 12, 14, 21, 18]
+
+# item = [1, 2, 3]
+# w = [20, 30, 50]
+# v = [5, 10, 15]
+
+set1 = set(items)
+efficiency = 0
+total_Value = 0
+earnings = 0
+max_value = 0
+
+for n in range(1, len(set1) + 1):
+    data = itertools.combinations(set1, n)
+    subsets = set(data)
+    efficiency += len(subsets)
+    for i in subsets:
+        total_Value = 0
+        earnings = 0
+        for j in i:
+            total_Value += w[int(j) - 1]
+            earnings += w[int(j) - 1] * v[int(j) - 1] / 100
+        if total_Value < 500:
+            print("subset: ", i, end=" ")
+            print("weight: ", total_Value, end=" ")
+            print("earnings: ", earnings)
+
+            if earnings > max_value:
+                max_value = earnings
+                winner_subset = i
+    print("\n")
+
+cost = 0
+for i in winner_subset:
+    cost += w[i - 1]
+print("cout total: ", cost)
+print("benefice: ", max_value)
+print("actions achetees: ", winner_subset)
+
+
+efficiency += len(subsets)
+print("\nefficiency: " + str(efficiency))
